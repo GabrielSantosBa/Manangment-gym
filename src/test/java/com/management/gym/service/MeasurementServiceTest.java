0aecdd922a71a200fc695a6ed42c98967a07b52d
GabrielSantosBa <gab.arteinfo@gmail.com>
2022-08-23 15:42:42 -0300

    TEST[Implementando Tests]: Testes da classe MonthlyPaymentService e MeasumentService concluidos com maxima cobertura

--- src/test/java/com/management/gym/service/MeasurementServiceTest.java
+++ src/test/java/com/management/gym/service/MeasurementServiceTest.java
@@ -1,22 +1,83 @@
 package com.management.gym.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.management.gym.builders.StudentBuilder;
+import com.management.gym.model.Student;
+import com.management.gym.repository.MeasurementRepository;
+import com.management.gym.repository.StudentRepository;
 
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class MeasurementServiceTest {
 
-	@Test
-	void testUpdateMeasumenteStudent() {
-		fail("Not yet implemented");
-	}
+	
+	@Mock
+	private StudentRepository studentRepository;
+	
+	@Mock
+	private MeasurementRepository measurementRepository;
 
+	@InjectMocks
+	private MeasurementService measurementService;
+	
+
+	private Student student;	
+	StudentBuilder studentBuilder = new StudentBuilder();	
+	
+	
+	@BeforeEach
+	public void setup() {
+		startStudentMock();
+	}
+	
+	
+	
 	@Test
-	void testMeasurementService() {
-		fail("Not yet implemented");
+	void testUpdateMeasumenteStudentWhenIdExists() {
+		var measurement = student.getMeasurements().get(0);
+		
+		when(studentRepository.existsById(student.getId())).thenReturn(true);
+		when(studentRepository.findById(student.getId())).thenReturn(Optional.of(student));
+		when(measurementRepository.save(measurement)).thenReturn(measurement);
+		when(studentRepository.save(student)).thenReturn(student);
+		
+		var studentUpdated = measurementService.updateMeasumenteStudent(student.getId(), measurement);
+		
+		assertNotNull(studentUpdated);
+		assertTrue(studentUpdated.getMeasurements().size() > 1);
+	}
+	
+	
+	@Test
+	void testUpdateMeasumenteStudentWhenIdNotExists() {
+		var measurement = student.getMeasurements().get(0);
+		when(studentRepository.existsById(Mockito.any())).thenReturn(false);
+		ResponseStatusException responseStatusException = assertThrows(ResponseStatusException.class, ()->  measurementService.updateMeasumenteStudent(student.getId(), measurement));
+		
+		assertTrue(responseStatusException.getMessage().contains("Student não encontrado!"));
+		assertTrue(responseStatusException.getStatus().equals(HttpStatus.NOT_FOUND));
+	}
+	
+	
+	@SuppressWarnings("static-access")
+	public void startStudentMock() {
+			student = studentBuilder.createStudent();
 	}
 
 }
