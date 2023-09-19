5699677b90d752a42b99ba21bb1070905b2a5f81
GabrielSantosBa <gab.arteinfo@gmail.com>
2023-09-19 11:01:19 -0300

    Correcao CORS

--- src/main/java/com/management/gym/config/CorsConfig.java
+++ src/main/java/com/management/gym/config/CorsConfig.java
@@ -1,12 +1,7 @@
 package com.management.gym.config;
 
-import java.util.Arrays;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,7 +13,11 @@ public class CorsConfig{
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(final CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("*").allowedHeaders("*");
+                registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT","DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
             }
         };
     }
--- src/test/java/com/management/gym/service/StudentServiceTest.java
+++ src/test/java/com/management/gym/service/StudentServiceTest.java
@@ -3,27 +3,21 @@ package com.management.gym.service;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.management.gym.builders.StudentBuilder;
 import com.management.gym.model.Student;
