5c3c7b5f12c5c4697736800f6a588afed7e8aaa5
GabrielSantosBa <gab.arteinfo@gmail.com>
2023-10-09 18:48:02 -0300

    Adiciona regra CORS para testes locais

--- src/main/java/com/management/gym/controller/StudentController.java
+++ src/main/java/com/management/gym/controller/StudentController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -33,6 +34,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/student")
 @RestController
+@CrossOrigin("http://localhost:4200/")
 public class StudentController {
 	
 	private final StudentService studentService;
