7654ac7e4133fd2871b27aee048debb762d06ffb
GabrielSantosBa <gab.arteinfo@gmail.com>
2023-10-22 18:33:13 -0300

    implementa novas regras CORS e swagger

--- src/main/java/com/management/gym/config/CorsConfig.java
+++ src/main/java/com/management/gym/config/CorsConfig.java
@@ -1,30 +1,48 @@
 package com.management.gym.config;
 
-import org.apache.catalina.filters.CorsFilter;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 @Configuration
-public class CorsConfig extends CorsFilter{
-
+public class CorsConfig {
 	
-	private static final long serialVersionUID = 1L;
+	
+	@Value("${personal.security.url-cors}")
+    private List<String> URLS_FROTEND;
 	
 	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				CorsRegistration cors = registry.addMapping("https://gym-management2-fjmn.vercel.app");				
-				cors.allowedMethods("*");
-				cors.allowedOrigins("*");
-				cors.allowedHeaders("*");
-				cors.allowCredentials(false);
-				cors.maxAge(3600);
-			}
-		};
-	}
+    CorsConfigurationSource corsConfigurationSource() {
+        System.out.println(this.URLS_FROTEND);
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(this.URLS_FROTEND);
+        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        //configuration.setExposedHeaders(List.of("x-auth-token"));
+	final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+	
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				CorsRegistration cors = registry.addMapping("https://gym-management2-fjmn.vercel.app");				
+//				cors.allowedMethods("*");
+//				cors.allowedOrigins("*");
+//				cors.allowedHeaders("*");
+//				cors.allowCredentials(false);
+//				cors.maxAge(3600);
+//			}
+//		};
+//	}
 }
